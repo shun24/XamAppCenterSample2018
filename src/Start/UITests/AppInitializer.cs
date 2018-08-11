7e690f4220bfd4164739251c5a06d183f79d7796
ShuntaroOgata <>
2018-08-11 17:36:30 +0900

    third

--- src/Start/UITests/AppInitializer.cs
+++ src/Start/UITests/AppInitializer.cs
@@ -1,1 +1,27 @@
-﻿ 
+﻿using Xamarin.UITest;
+
+namespace XamAppCenterSample2018.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .EnableLocalScreenshots()
+                    .PreferIdeSettings()
+                    .InstalledApp("com.shun2420180811.XamAppCenterSample2018")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .EnableLocalScreenshots()
+                .PreferIdeSettings()
+                .InstalledApp("com.shun2420180811.XamAppCenterSample2018")
+                .StartApp();
+        }
+    }
+}
